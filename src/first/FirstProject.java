3acf9f6c80cd03c6b51950d4e9df87507e4a0ec9
hunostor <Noxoil78->
2018-04-20 19:10:37 +0200

    Ternary op

--- src/first/FirstProject.java
+++ src/first/FirstProject.java
@@ -6,6 +6,19 @@ public class FirstProject {
 		
 		Human first = new Human();
 		
-	    first.writeMyName();
+		first.setName("Gyula");
+		first.setAge(40);
+		
+		
+		
+		System.out.println(first.getName() == null ? "Ures" : " Nem ures: "+first.getName());
+		
+//	    if(first.getName() == null) {
+//	    	System.out.println("nincs nev");
+//	    } else {
+//	    	System.out.println("van nev: "+first.getName());
+//	    }
+	    
+	    
 	}
 }
--- src/first/Human.java
+++ src/first/Human.java
@@ -1,9 +1,26 @@
 package first;
 
 public class Human {
-	String name = "Gyula";
+	private String name;
+	private int age;
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
 	
 	void writeMyName() {
 		System.out.println("Az en nevem: "+name);
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
 	}
 }
